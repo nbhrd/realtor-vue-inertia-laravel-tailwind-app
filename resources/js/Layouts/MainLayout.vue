42f32682bffdbaa02be7acc6e0fae39a4df517d2
nbhrd <nh@mail.com>
2024-09-26 20:50:06 +0900

    売却物件(Listing)CRUD画面デザイン修正

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <header
-        class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full"
+        class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full"
     >
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
@@ -13,11 +13,9 @@
                     <Link :href="route('listing.index')">Listings App</Link>
                 </div>
                 <div>
-                    <Link
-                        :href="route('listing.create')"
-                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded"
-                        >+ New Listing</Link
-                    >
+                    <Link :href="route('listing.create')" class="btn-primary">
+                        + New Listing
+                    </Link>
                 </div>
             </nav>
         </div>
