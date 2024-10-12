afe0c30eba5f1bac9ae70762ab9c45a2b5991efa
nbhrd <nh@mail.com>
2024-10-12 20:38:48 +0900

    listingsテーブルに外部キー追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        \App\Models\Listing::factory(20)->create();
+
+        User::factory()->create([
+            'name' => 'Test User2',
+            'email' => 'test2@example.com',
+        ]);
+
+        \App\Models\Listing::factory(20)->create([
+            'by_user_id' => 1,
+        ]);
     }
 }
